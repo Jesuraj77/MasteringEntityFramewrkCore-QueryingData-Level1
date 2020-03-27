caca5b77546027d3507ae123cf496224b09bea60
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:20:09 +0530

    files added

---
+++ Level1.BusinessLayer/Services/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Level1.BusinessLayer.Services
+{
+    public class Class
+    {
+        //Write a query to display the records in the table using Query syntax.
+    }
+}
